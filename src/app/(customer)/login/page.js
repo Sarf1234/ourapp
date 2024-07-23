db8f746c3fbcf40199c05886bceb5be38ba649e1
Sarf1234 <sarfarajshekh69@gmail.com>
2024-07-23 23:46:08 +0530

    some change

--- src/app/(customer)/login/page.js
+++ src/app/(customer)/login/page.js
@@ -1,90 +1,89 @@
-'use client'
-import Link from 'next/link';
+import { useSession, signIn } from 'next-auth/react';
 
+const LoginPage = () => {
+  const { data: session, status } = useSession();
 
-const page = () => {
-  
-  const Submit = async () =>{ 
-    const data = {
-      user_id : email,
-      password : password
-    } 
+  if (status === 'authenticated') {
+    // User is already logged in, redirect to protected route
+    return <Redirect to="/protected" />;
+  }
 
-    try{
-       const response =  await fetch(`${process.env.NEXT_PUBLIC_URL}/login`,{
-            method:"POST",
-            body:JSON.stringify(data),
-            headers: {
-              'Authorization': GetAuthorization()
-          }
-        })
-        if(!response.ok){
-            if(response?.status===401){
-             alert("invaild password!")
-            }
-            else
-            {
-              alert("some error occure");
-            }  
-        }else{
-            const datacookies = await response.json()
-            console.log(datacookies)
-        }             
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    const email = event.target.email.value;
+    const password = event.target.password.value;
+
+    try {
+      const response = await fetch(`${process.env.NEXT_PUBLIC_URL}/login`, {
+        method: 'POST',
+        body: JSON.stringify({ user_id: email, password }),
+        headers: {
+          'Authorization': GetAuthorization()
+        }
+      });
+
+      if (!response.ok) {
+        if (response.status === 401) {
+          alert("Invalid password!");
+        } else {
+          alert("Some error occurred");
+        }
+      } else {
+        const data = await response.json();
+        signIn('credentials', { email, password, callbackUrl: '/protected' });
+      }
+    } catch (error) {
+      console.log(error);
     }
-    catch(error){
-        console.log(error)
-    }
-       
-}
-
+  };
 
   return (
     <div className="flex justify-center items-center h-5/6 bg-gradient-to-br from-gray-800 to-gray-900">
       <div className="w-full max-w-md bg-white rounded-lg p-8 shadow-md">
         <h1 className="text-3xl font-semibold text-center mb-4 text-black tracking-widest">eRentals</h1>
-        <div className='text-left'>
-          <label className='text-black text-left font-medium'>Email</label>
-          <input
-            type="email"
-            placeholder="Email"
-            className="w-full px-3 py-2 mb-4 text-black bg-gray-100 border border-blue-300 rounded-md focus:outline-none focus:border-blue-500"
-            // value={email}
-            // onChange={(e) => setEmail(e.target.value)}
-          />
-        </div>
-        <div className='text-left'>
-          <label className='text-black text-left font-medium'>Password</label>
-          <input
-            type="password"
-            placeholder="Password"
-            className="w-full px-3 py-2 mb-6 bg-gray-100 text-black  border border-gray-300 rounded-md focus:outline-none focus:border-blue-500"
-            // value={password}
-            // onChange={(e) => setPassword(e.target.value)}
-          />
-        </div>
-        <button
-          className="w-full bg-blue-site-color text-white font-semibold px-4 py-2 rounded-md hover:bg-blue-600 focus:outline-none focus:bg-blue-600"
-          // onClick={handleLogin}
-        >
-          Sign In
-        </button>
-        <div className="mt-4 text-left">
-          <p className="text-sm text-black">
-            Don't have an account?{' '}
-            <Link href="#" className="text-red-500 hover:underline">
-              Sign Up
-            </Link>
-          </p>
-          <p className="text-sm mt-2 text-black">
-            Forgot your password?{' '}
-            <Link href="#" className="text-red-500 hover:underline">
-              Reset Password
-            </Link>
-          </p>
-        </div>
+        <form onSubmit={handleSubmit}>
+          <div className='text-left'>
+            <label className='text-black text-left font-medium'>Email</label>
+            <input
+              type="email"
+              placeholder="Email"
+              className="w-full px-3 py-2 mb-4 text-black bg-gray-100 border border-blue-300 rounded-md focus:outline-none focus:border-blue-500"
+              name="email"
+            />
+          </div>
+          <div className='text-left'>
+            <label className='text-black text-left font-medium'>Password</label>
+            <input
+              type="password"
+              placeholder="Password"
+              className="w-full px-3 py-2 mb-6 bg-gray-100 text-black  border border-gray-300 rounded-md focus:outline-none focus:border-blue-500"
+              name="password"
+            />
+          </div>
+          <button
+            className="w-full bg-blue-site-color text-white font-semibold px-4 py-2 rounded-md hover:bg-blue-600 focus:outline-none focus:bg-blue-600"
+          >
+            Sign In
+          </button>
+          <div className="mt-4 text-left">
+            <p className="text-sm text-black">
+              Don't have an account?{' '}
+              <Link href="#" className="text-red-500 hover:underline">
+                Sign Up
+              </Link>
+            </p>
+            <p className="text-sm mt-2 text-black">
+              Forgot your password?{' '}
+              <Link href="#" className="text-red-500 hover:underline">
+                Reset Password
+              </Link>
+            </p>
+          </div>
+        </form>
       </div>
     </div>
   );
-}
+};
 
-export default page
+export default LoginPage;
--- src/auth.js
+++ src/auth.js
@@ -1,6 +1,65 @@
-import NextAuth from "next-auth"
+import NextAuth, { CredentialsSignin } from "next-auth"
 import Credentials from "next-auth/providers/credentials"
  
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [],
+  providers: [
+    Credentials({
+      credentials: {
+        username: { label: "Username" },
+        password: { label: "Password", type: "password" },
+      },
+      authorize: async (credentials) => {
+        const user_id = credentials.username
+        const password = credentials.password;
+      
+        if (!user_id ||!password) {
+          throw new CredentialsSignin("please enter your username and password")
+        }
+      
+        try {
+          const response = await fetch(`${process.env.NEXT_PUBLIC_URL}/login`, {
+            method: "POST",
+            body: JSON.stringify({ user_id, password }),
+          })
+      
+          if (!response.ok) {
+            throw new Error("Couldn't login with username and password")
+          }
+      
+          const tokenResponse = await response.json()
+          const token = tokenResponse.authToken
+          
+          const userDetailsResponse = await fetch(`${process.env.NEXT_PUBLIC_URL}/user`, {
+            method: "GET",
+            headers: {
+              'Content-Type': 'application/json',
+              Authorization: `Bearer ${token}`,
+            },
+          })
+
+          if (!userDetailsResponse.ok) {
+            throw new Error("Couldn't fetch user details")
+          }
+
+          const userData = await userDetailsResponse.json()
+            return {
+              id: userData.id,
+              role: userData?.roles[0],
+              email: userData.email,
+              mobile: userData.mobile,
+              firstName: userData.f_name,
+              lastName: userData.l_name,
+              profession:userData.profession,
+              varified: userData.is_varified,
+              profilPick: userData.profile_pic,
+            }
+        } catch (error) {
+          console.log(error)
+        }
+      }
+    }),
+  ],
+  pages:{
+    signIn:"/login"
+  }
 })
