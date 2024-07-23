211375209e88d89c32eed449376a8f8394d4d2a0
Sarf1234 <sarfarajshekh69@gmail.com>
2024-07-23 15:47:40 +0530

    some change

--- src/ui/DropDown.jsx
+++ src/ui/DropDown.jsx
@@ -36,85 +36,13 @@ const DropDown = () => {
           </button>
         </div>
         {isOpen && (
-          <div
-            className="absolute right-0 z-10 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
-            role="menu"
-            aria-orientation="vertical"
-            aria-labelledby="menu-button"
-            tabindex="-1"
-          >
-            <div className="py-1" role="none">
-              <a
-                href="#"
-                className="block px-4 py-2 text-sm text-gray-700"
-                role="menuitem"
-                tabindex="-1"
-                id="menu-item-0"
-              >
-                Edit
-              </a>
-              <a
-                href="#"
-                className="block px-4 py-2 text-sm text-gray-700"
-                role="menuitem"
-                tabindex="-1"
-                id="menu-item-1"
-              >
-                Duplicate
-              </a>
-            </div>
-            <div className="py-1" role="none">
-              <a
-                href="#"
-                className="block px-4 py-2 text-sm text-gray-700"
-                role="menuitem"
-                tabindex="-1"
-                id="menu-item-2"
-              >
-                Archive
-              </a>
-              <a
-                href="#"
-                className="block px-4 py-2 text-sm text-gray-700"
-                role="menuitem"
-                tabindex="-1"
-                id="menu-item-3"
-              >
-                Move
-              </a>
-            </div>
-            <div className="py-1" role="none">
-              <a
-                href="#"
-                className="block px-4 py-2 text-sm text-gray-700"
-                role="menuitem"
-                tabindex="-1"
-                id="menu-item-4"
-              >
-                Share
-              </a>
-              <a
-                href="#"
-                className="block px-4 py-2 text-sm text-gray-700"
-                role="menuitem"
-                tabindex="-1"
-                id="menu-item-5"
-              >
-                Add to favorites
-              </a>
-            </div>
-            <div className="py-1" role="none">
-              <Link
-                href="/login"
-                className="block px-4 py-2 text-sm text-gray-700"
-                role="menuitem"
-                tabindex="-1"
-                id="menu-item-6"
-              >
-                Login
-              </Link>
-            </div>
-          </div>
+         <div class="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabindex="-1">
+         <div class="py-1" role="none">
+           <Link href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="menu-item-0">Account settings</Link>
+           <Link href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="menu-item-1">Support</Link>
+           <Link href="/login" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="menu-item-2">Login</Link>
+         </div>
+       </div>
         )}
       </div>
     </div>
