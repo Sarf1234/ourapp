b80b6d9c8efe7c65f705ad5e2812fef1208e167c
Sarf1234 <sarfarajshekh69@gmail.com>
2024-07-23 11:40:19 +0530

    some change

--- src/app/(customer)/login/page.js
+++ src/app/(customer)/login/page.js
@@ -1,16 +1,10 @@
 'use client'
-import { useState } from 'react'
 import Link from 'next/link';
-import useUserloginStatus from '@/context/UserloginStatus'
-import { CheckAuth } from '@/utility/CheckAuth';
-import GetAuthorization from '@/utility/GetAuthorization';
-import Cookies from 'js-cookie';
+
 
 const page = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const {setisloggedin, setloginRole} = useUserloginStatus()
-  console.log(CheckAuth())
   
   const Submit = async () =>{ 
     const data = {
@@ -36,9 +30,7 @@ const page = () => {
             }  
         }else{
             const datacookies = await response.json()
-            setCookie(datacookies)
-            setisloggedin(CheckAuth())
-            await fetchData()
+            console.log(datacookies)
         }             
     }
     catch(error){
@@ -47,56 +39,6 @@ const page = () => {
        
 }
 
-const fetchData = async() =>{ 
-  if(CheckAuth()){ 
-      try{
-          const response =  await fetch(`${process.env.NEXT_PUBLIC_URL}/user`,{
-              method:'GET',
-              headers:{
-                  'Content-Type': 'application/json',
-                  'Authorization': GetAuthorization()
-              }
-          })
-          if(!response.ok){
-              console.log("something went wrong")
-              return null;
-              }else{
-                const data = await response.json()
-                const role = data.roles[0];
-                setloginRole(role);
-                localStorage.setItem('userRole', role);
-                    if (data.roles[0] === "admin") {
-                      console.log("admin")
-                    }
-                    else if(data.roles[0] === "vendor"){
-                      console.log("vendor")
-                    } else {
-                      console.log("user")
-                    }
-           }
-
-         }catch(e){
-           console.log("something went wrong ")
-         }                               
-  }
-}
-  
-  const handleLogin = async() => {
-    if(email === ""){
-      alert("Please enter email");
-     }         
-     else if(password === ""){
-      alert("Please enter password");
-       } 
-      else{
-        await Submit();
-      }
-  };
-
-  const setCookie = (cookieData) =>{
-    Cookies.set('authToken',cookieData?.authToken,{path:'/'})
-    Cookies.set('refreshToken',cookieData?.refreshToken,{path:'/'})   
-  } 
 
   return (
     <div className="flex justify-center items-center h-5/6 bg-gradient-to-br from-gray-800 to-gray-900">
